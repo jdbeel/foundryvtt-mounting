f78610698d587f24f77b30d637fb7e0ae4c41f9f
Jacob Beel <jbeel3@gatech.edu>
2022-09-24 15:54:34 -0400

    Update to be compatible with data module.

--- src/module/hud.ts
+++ src/module/hud.ts
@@ -1,7 +1,8 @@
 import { Mounting } from './mounting.js';
 import { getCanvas, getGame, getToken } from './utils.js';
 
-import { MODULE_ID } from './const';
+import { MODULE_ID, RIDER_PROPERTY_NAME } from './const';
+import { MountData, RiderData } from './data.js';
 
 // Temporary shim
 type hudToken = {
@@ -46,38 +47,33 @@ export class MountingHud {
 
     static addButton(app: TokenHUD, html: JQuery, hudToken: hudToken) {
         let button: JQuery;
-        let riderToken: Token | undefined, mountToken: Token | undefined;
+        let riderToken: Token, mountToken: Token;
         let mounted: boolean;
 
-        const selectedToken = getToken(hudToken._id);
-        if (selectedToken == undefined) {
-            ui?.notifications?.error('Error selecting tokens.');
-            return;
-        }
-
-        if (hasProperty(selectedToken?.document, `flags.${MODULE_ID}.mount_id`)) {
-            riderToken = selectedToken;
-            mountToken = getToken(
-                // @ts-ignore
-                selectedToken?.document.getFlag(MODULE_ID, 'mount_id'),
-            );
-            mounted = true;
-        } else if (hasProperty(selectedToken?.document, `flags.${MODULE_ID}.rider_id`)) {
-            riderToken = getToken(
-                // @ts-ignore
-                selectedToken?.document.getFlag(MODULE_ID, 'rider_id'),
-            );
-            mountToken = selectedToken;
-            mounted = true;
-        } else {
-            riderToken = selectedToken;
-            mountToken = getCanvas().tokens?.controlled.find((t) => t.id != selectedToken?.id);
+        const riderData = RiderData.fromTokenId(hudToken._id);
+        if (riderData == undefined) {
             mounted = false;
+            let selectedToken = getToken(hudToken._id);
+            if (selectedToken != undefined)
+                riderToken = selectedToken;
+            else
+                return; // TODO error.
+            
+            selectedToken = getCanvas().tokens?.controlled.find((t) => t.id != selectedToken?.id);
+            if (selectedToken != undefined)
+                mountToken = selectedToken;
+            else
+                return; // TODO error.
+        } else {
+            mounted = true;
+            riderToken = riderData.getToken();
+            const mountData = MountData.fromTokenId(riderData.mountId);
+            mountToken = mountData.getToken();
         }
 
         const tooltipData = {
-            rider_name: riderToken?.name,
-            mount_name: mountToken?.name,
+            rider_name: riderToken.name,
+            mount_name: mountToken.name,
         };
 
         if (mounted === true) {
@@ -87,28 +83,30 @@ export class MountingHud {
         }
 
         const errorData = {
-            rider_id: riderToken?.id,
-            mount_id: mountToken?.id,
+            rider_id: riderToken.id,
+            mount_id: mountToken.id,
         };
 
         button.find('i').on('click', async function (event) {
-            if (riderToken?.document.getFlag(MODULE_ID, 'mount_id') != undefined) {
+            if (riderToken.document.getFlag(MODULE_ID, RIDER_PROPERTY_NAME) != undefined) {
+                console.log(`${MODULE_ID} | DISMOUNT BUTTON CLICKED`); // TODO localization?
                 // Attempt to dismount...
                 await Mounting.unmount(riderToken);
                 // If dismount succeeded, then update the hud.
                 // Otherwise, print an error.
-                if (riderToken?.document.getFlag(MODULE_ID, 'mount_id') == undefined) {
+                if (riderToken.document.getFlag(MODULE_ID, RIDER_PROPERTY_NAME) == undefined) {
                     MountingHud.removeSlash(button);
                     MountingHud.setTooltip(button, getGame().i18n.format('MOUNTING.info.MountTooltip', tooltipData));
                 } else {
                     ui?.notifications?.error(getGame().i18n.format('MOUNTING.error.DismountUnsuccessful', errorData));
                 }
             } else {
+                console.log(`${MODULE_ID} | MOUNT BUTTON CLICKED`); // TODO localization?
                 // Attempt to mount...
                 await Mounting.mount(riderToken);
                 // If mount succeeded, then update the hud.
                 // Otherwise, print an error.
-                if (riderToken?.document.getFlag(MODULE_ID, 'mount_id') != undefined) {
+                if (riderToken.document.getFlag(MODULE_ID, RIDER_PROPERTY_NAME) != undefined) {
                     MountingHud.addSlash(button);
                     MountingHud.setTooltip(button, getGame().i18n.format('MOUNTING.info.DismountTooltip', tooltipData));
                 } else {
